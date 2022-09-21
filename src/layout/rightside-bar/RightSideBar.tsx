becb68e2b6a1e02b92f47a19d2fc27cd1cc51261
Berengaar <emir97han@gmail.com>
2022-09-22 02:44:29 +0300

    Searchbar created

--- src/layout/rightside-bar/RightSideBar.tsx
+++ src/layout/rightside-bar/RightSideBar.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import SearchBar from './SearchBar'
 
 const RightSideBar = () => {
     return (
         <div className='flex flex-col w-3/12 h-screen bg-white border-l shadow-md '>
-            Rightbar
+            <SearchBar />
         </div>
     )
 }
